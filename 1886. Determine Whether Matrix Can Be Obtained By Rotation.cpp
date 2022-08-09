a56c91d4427291699d83bd9b88fcfaebdfae99fc
Yash <86280730+yashpatel137@users.noreply.github.com>
2022-08-09 11:37:36 +0530

    Create 1886. Determine Whether Matrix Can Be Obtained By Rotation.cpp

---
+++ 1886. Determine Whether Matrix Can Be Obtained By Rotation.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& mat)
+    {
+        reverse(begin(mat),end(mat));
+        for(int i=0;i<mat.size();i++)
+        {
+            for(int j=i+1;j<mat.size();j++)
+            {
+                swap(mat[i][j],mat[j][i]);
+            }
+        }
+    }
+    bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
+        for(int i=0;i<4;i++)
+        {
+            if(mat==target)
+            {
+                return true;
+            }
+            rotate(mat);
+        }
+        return false;
+    }
+};
